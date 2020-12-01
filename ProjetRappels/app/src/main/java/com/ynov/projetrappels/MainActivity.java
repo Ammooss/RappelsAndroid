cfb540a61addba59712b2966cdb43a1c0b2a7a96
Maxence <33833639+Ammooss@users.noreply.github.com>
2020-12-01 12:43:40 +0100

    Add files via upload

--- ProjetRappels/app/src/main/java/com/ynov/projetrappels/MainActivity.java
+++ ProjetRappels/app/src/main/java/com/ynov/projetrappels/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference firebasedb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebasedb = FirebaseDatabase.getInstance().getReference();
+
+        String id = "02";
+        String titre = "DEBOUT";
+
+        creeRappel(id, titre);
+    }
+
+    public void creeRappel (String id, String titre) {
+        Rappel rappel = new Rappel (titre);
+        firebasedb.child("rappels").child(id).setValue(rappel);
+        //firebasedb.child("rappels").child(id).child("titre").setValue("f");
     }
 }
---
+++ ProjetRappels/app/src/main/java/com/ynov/projetrappels/Rappel.java
@@ -0,0 +1,16 @@
+package com.ynov.projetrappels;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Rappel {
+    public String titre;
+
+    public Rappel () {
+
+    }
+    public Rappel (String titre) {
+        this.titre = titre;
+    }
+}
+
